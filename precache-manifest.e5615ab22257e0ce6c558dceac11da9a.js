450d3fe4e1a19b3a3beab0595c30c72820f04c96
Admin <you@example.com>
2020-01-09 22:33:53 +0300

    Updates

---
+++ precache-manifest.e5615ab22257e0ce6c558dceac11da9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effbf7d2b3e46fcfcddbfe48d2a72d30",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "d241f19556c5840cd8c5",
+    "url": "/react-deploy/static/css/main.608ae1ea.chunk.css"
+  },
+  {
+    "revision": "b1e4ee64ed2ae49ec5ed",
+    "url": "/react-deploy/static/js/2.13146678.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-deploy/static/js/2.13146678.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d241f19556c5840cd8c5",
+    "url": "/react-deploy/static/js/main.dc409709.chunk.js"
+  },
+  {
+    "revision": "dcf075223ef7849501c1",
+    "url": "/react-deploy/static/js/runtime-main.02e4e608.js"
+  }
+]);
